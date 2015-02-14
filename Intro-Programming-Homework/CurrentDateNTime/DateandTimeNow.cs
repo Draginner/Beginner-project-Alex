d33153db601db7dd68716f2c476683d88df12bee
Alex <drakinar1@gmail.com>
2015-02-14 18:34:30 +0200

    no message

---
+++ Intro-Programming-Homework/CurrentDateNTime/DateandTimeNow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentDateNTime
+{
+    class DateandTimeNow
+    {
+        static void Main(string[] args)
+        {
+            DateTime savenow = DateTime.Now;
+            Console.WriteLine(savenow);
+        }
+    }
+}
