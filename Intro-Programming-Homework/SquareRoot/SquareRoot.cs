55ccf4a51dad8f2f59270803dcbeb67a713ffa13
Alex <drakinar1@gmail.com>
2015-02-14 16:24:36 +0200

    no message

---
+++ Intro-Programming-Homework/SquareRoot/SquareRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+         double a =  Math.Sqrt(12345);
+            Console.WriteLine(a);
+
+        }
+    }
+
