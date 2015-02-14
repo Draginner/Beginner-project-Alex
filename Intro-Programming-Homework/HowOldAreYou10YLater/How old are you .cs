3bc3bca23cec352739ea148aa1e3fe6f08eac895
Alex <drakinar1@gmail.com>
2015-02-14 18:45:18 +0200

    no message

---
+++ Intro-Programming-Homework/HowOldAreYou10YLater/How old are you .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOldAreYou10YLater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            Console.WriteLine("Write your age:");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is {0}", Age);
+            Age = Age + 10;
+            Console.WriteLine("You will be  {0} years old after 10 years",Age );
+        }
+    }
+}
