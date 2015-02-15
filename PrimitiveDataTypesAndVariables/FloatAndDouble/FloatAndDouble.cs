8a1e3037154427f7e886b2024ce72f4f9dfb8093
Alex <drakinar1@gmail.com>
2015-02-15 21:29:14 +0200

    hoho

---
+++ PrimitiveDataTypesAndVariables/FloatAndDouble/FloatAndDouble.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatAndDouble
+{
+    class FloatAndDouble
+    {
+        static void Main(string[] args)
+        {
+            float FirstNumber = 12.345F;
+            float SecondNumber = 3456.091F;
+            double ThirdNumber =8923.1234857;
+            double ForthNumber = 34.567839023;
+            Console.WriteLine("{0}| {1}| {2}| {3}",FirstNumber,SecondNumber,ThirdNumber,ForthNumber);
+        }
+    }
+}
