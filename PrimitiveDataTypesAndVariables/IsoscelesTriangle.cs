96e61afe582a58b5e5044729f26a8332c3e8323d
Alex <drakinar1@gmail.com>
2015-02-19 14:54:00 +0200

    no message

---
+++ PrimitiveDataTypesAndVariables/IsoscelesTriangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoscelesTriangle
+{
+    class IsoscelesTriangle
+    {
+        static void Main(string[] args)
+        {
+            string copyRight = "\u00A9";
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Console.WriteLine("   {0}",copyRight);
+            Console.WriteLine("  " + copyRight + " " + copyRight);
+            Console.WriteLine(" " + copyRight+ "   "  + copyRight);
+            Console.WriteLine(copyRight+" " +copyRight+" "+copyRight+" "+copyRight);
+        }
+    }
+}
