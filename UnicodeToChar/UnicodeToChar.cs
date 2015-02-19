98f33d7400d9eed190e5a0517520e34c2c336dd6
Alex <drakinar1@gmail.com>
2015-02-19 12:29:20 +0200

    PrimitiveDataTypes

---
+++ UnicodeToChar/UnicodeToChar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeToChar
+{
+    class UnicodeToChar
+    {
+        static void Main(string[] args)
+        {
+            char unicodeChar;
+            unicodeChar = '\u0044';
+            Console.WriteLine(unicodeChar);
+        }
+    }
+}
---
+++ VariableInHexadecimal/Transform to Hexadecimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableInHexadecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 254;
+            string transformToHexa = "FE";
+            Console.WriteLine("We transform the number {0} to hexadecimal and it is {1}",value,transformToHexa);
+        }
+    }
+}
