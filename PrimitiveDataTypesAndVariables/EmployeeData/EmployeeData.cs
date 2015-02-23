69d3cbd1ebe792038ae4487294a9108f261fb02c
Alex <drakinar1@gmail.com>
2015-02-23 17:42:07 +0200

    no message

---
+++ PrimitiveDataTypesAndVariables/EmployeeData/EmployeeData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeData
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {int x;
+        string firstName = "Aleksandar";
+            string lastName = "Tenchev";
+            short age = 25;
+            char gander = 'M' ;
+
+            long personalIDNumber = 8306112507;
+            int uniqueEmployeeNumber = 27560000;
+            Console.WriteLine("Firstname of the worker:{0}",firstName);
+            Console.WriteLine("Lastname of the worker:{0}",lastName);
+            Console.WriteLine("Age of the worker:{0}",age);
+            
+            Console.WriteLine("Gander: {0} ",gander);
+            Console.WriteLine("ID number of the worker: {0}",personalIDNumber);
+            Console.WriteLine("Worker's number: {0}", uniqueEmployeeNumber);
+            
+        }
+    }
+}
