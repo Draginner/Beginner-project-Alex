8983b11ed821ddd0d8ef712e779536fe47b48f09
Alex <drakinar1@gmail.com>
2015-02-23 17:35:27 +0200

    no message

---
+++ OperatorsAndExpressions/OddOrEven/OddOrEven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    class OddOrEven
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Write a number to check if its odd or even.");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} = {1} ",number , 0 == number % 2 );
+        }
+    }
+}
